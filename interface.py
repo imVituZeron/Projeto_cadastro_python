5c746ac3e2fa4f72b20b52c37e06c730da2c9b14
Vitor Santos <vitorpaulasantos@gmail.com>
2020-07-06 22:19:58 -0300

    criando um campo de texte, e um button para confirmar a opção

--- interface.py
+++ interface.py
@@ -2,20 +2,28 @@ import tkinter as tk
 
 root = tk.Tk()
 
-def opcoes(root, text, color, linha, coluna):
+def text_menu(root, text, color, linha, coluna):
     op_label = tk.Label(root, text=text ,bg=color)
     op_label.grid(row=linha, column=coluna)
 
+def opcoes(stringvar: tk.StringVar, button: tk.Button):
+    pass
 
 main_title = tk.Label(root, text='Cadastre-se', bg='#fff', font=('Helvetica',12,'bold'))
 main_title.grid(row=0, column=0, pady=(0, 20))
 
 # menu principal
-opcoes(root, '1 - Cadastrar', '#fff', 1, 0)
-opcoes(root, '2 - Ler', '#fff', 2, 0)
-opcoes(root, '3 - Sair', '#fff', 3, 0)
+text_menu(root, '1 - Cadastrar', '#fff', 1, 0)
+text_menu(root, '2 - Ler', '#fff', 2, 0)
+text_menu(root, '3 - Sair', '#fff', 3, 0)
 
+entry_stringvar = tk.StringVar()
+entry = tk.Entry(root, bd=3, textvariable=entry_stringvar)
+entry.grid(row=4, column=0)
 
+button = tk.Button(root, text='Confirmar')
+button.grid(row=5, column=0)
+button.config(command=lambda: opcoes(entry_stringvar, button))
 
 root.title('Cadastro de Pessoas')
 root.config(bg='#fff', padx=20, pady=20)
